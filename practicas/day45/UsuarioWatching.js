865251dcf18b51248e7e84a6923b528f3ed4028f
Mauricio <mauricioguillermomorales@gmail.com>
2012-01-02 13:53:07 -0300

    Complete day 4 & 5 hasta el ejercicio 11 (Module y mixin)

--- practicas/day45/UsuarioWatching.js
+++ practicas/day45/UsuarioWatching.js
@@ -1,5 +1,8 @@
 function watchMoves(nombre) {
-    this.nombre = nombre
+    this.nombre = nombre,
+    this.getNombre = function (){
+		return this.nombre;	
+	}
 };
 watchMoves.prototype.verPeli = function(peli) {
     		console.log(this.nombre + " mirando: " + peli);
